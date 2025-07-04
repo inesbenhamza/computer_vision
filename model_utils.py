cc61f1eed313be4091a9424f8b0e464887c34652
Inès Ben Hamza <inesbenhamza@Iness-MacBook-Pro.local>
2025-07-04 22:01:58 +0200

    Ajout

--- model_utils.py
+++ model_utils.py
@@ -2,7 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from torchvision.utils import make_grid
+
 
 def save_model(model, path="simple_cnn.pth"):
     torch.save(model.state_dict(), path)
@@ -34,11 +34,4 @@ def evaluate_model(model, dataloader, class_names):
     print("\n Confusion Matrix:")
     print(confusion_matrix(all_labels, all_preds))
 
-def show_batch(images, labels, class_names):
-    img_grid = make_grid(images, nrow=8, normalize=True)
-    np_img = img_grid.numpy().transpose((1, 2, 0))
-    plt.figure(figsize=(12, 4))
-    plt.imshow(np_img)
-    plt.title(" | ".join([class_names[l] for l in labels]))
-    plt.axis('off')
-    plt.show()
+
